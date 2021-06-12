dc292abd0159df54a46d6ea2584413e29726e5ee
Juris-Berkulis <krztfjrbhm1@jmail.com>
2021-06-12 13:34:46 +0300

    1. Comment on the solution of all problems from the second lesson; 2. Change the solution to the first problem from the third lesson; 3. Complete the solution to the third problem from the third lesson.

--- scripts/lesson_03.js
+++ scripts/lesson_03.js
@@ -2,13 +2,22 @@ document.write('<h2>Lesson 3</h2>');
 
 document.write('<p><b>1. С помощью цикла while вывести все простые числа в промежутке от 0 до 100</b></p>');
 
-var a = 0;
+var a = 2;
 while (a <= 100) {
-    if (a == 100) {
-        document.write('<span>' + a + '.</span>');
-    }
-    else {
-        document.write('<span>' + a + '; </span>');
+    var i = 2;
+    while (i <= a) {
+        if (a % i == 0 && a != i) {
+            break;
+        }
+        if (a == i) {
+            if (a < 100) {
+                document.write('<span>' + a + '; </span>');
+            }
+            else if (a == 100) {
+                document.write('<span>' + a + '.</span>');
+            }
+        }
+        i++;
     }
     a++;
 }
@@ -20,12 +29,17 @@ document.write('<p><b>3. Товары в корзине хранятся в ма
 document.write('<p><b>a) Организовать такой массив для хранения товаров в корзине;</b></p>');
 
 
-var min = 1;
-var max = 11;
-var a = Math.floor(min + Math.random() * (max - min + 1));
-var b = Math.floor(min + Math.random() * (max - min + 1));
-var c = Math.floor(min + Math.random() * (max - min + 1));
-var basket = [{product: 'milk', price: a * 100}, {product: 'Tomatoes', price: b * 100}, {product: 'Potato', price: c * 100}];
+var minPr = 1;
+var maxPr = 10;
+var aPr = Math.floor(minPr + Math.random() * (maxPr - minPr + 1));
+var bPr = Math.floor(minPr + Math.random() * (maxPr - minPr + 1));
+var cPr = Math.floor(minPr + Math.random() * (maxPr - minPr + 1));
+var minCount = 1;
+var maxCount = 10;
+var aCount = Math.floor(minCount + Math.random() * (maxCount - minCount + 1));
+var bCount = Math.floor(minCount + Math.random() * (maxCount - minCount + 1));
+var cCount = Math.floor(minCount + Math.random() * (maxCount - minCount + 1));
+var basket = [{product: 'milk', price: aPr * 100, count: aCount}, {product: 'Tomatoes', price: bPr * 100, count: bCount}, {product: 'Potato', price: cPr * 100, count: cCount}];
 
 document.write('<p><b>b) Организовать функцию countBasketPrice, которая будет считать стоимость корзины.</b></p>');
 
@@ -33,8 +47,8 @@ var sum = 0;
 function countBasketPrice(basket) {
     document.write('<span>Список товаров: [</span>');
     for (var item of basket) {
-        sum += item.price;
-        document.write('<span>{' + item.product + ': ' + item.price + '}, </span>');
+        sum += item.price * item.count;
+        document.write('<span>{' + item.product + ': ' + item.count + ' шт. по цене ' + item.price + ' руб.}, </span>');
     }
     document.write('<span>]</span>');
     document.write('<p>Цена за все товары: ' + sum + '.</p>');
